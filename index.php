cb2e45441d3baf8500bbbb7d1f4a7800b7bf9663
Manoj-PC\Manoj <manoj.kumar@web2byte.com>
2017-05-02 12:46:01 +0530

    remove html tag

--- index.php
+++ index.php
@@ -1,45 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<?php  
-	$x = 5 + 6;
-	$text1 = "<br>this is sample var";
-
-	echo "<br>$x" .$text1. "!";
-	?>
-
 <?php 
-	function myfunction() {  
-		static $x = 0;
-		echo $x;
-		$x++;
-	}
-	echo "<br>";
-	myfunction();
-	echo "<br>";
-	myfunction();
-	echo "<br>";
-	myfunction();
-	echo "<br>";
- # this is single line comment in php 
-/* this milti line comment */
-#this is for test request
+    #this is php example for beginner
 
+    $x = 5 + 6;
+    $text1 = "<br>this is sample var";
 
-$a = 115;
-$b = 30;
+    echo "<br>$x" .$text1. "!";
 
-if ($a == 15) {
-	print "<br>this is right answer!!!";
-}
-else{
-	print "this is wrong answer!!!";
-}
+    function myfunction() {  
+        static $x = 0;
+        echo $x;
+        $x++;
+    }
+    echo "<br>";
+    myfunction();
+    echo "<br>";
+    myfunction();
+    echo "<br>";
+    myfunction();
+    echo "<br>";
 
-?>
+    # this is single line comment in php 
+    /* this milti line comment */
+    #this is for test request
 
-</body>
-</html>
+    $a = 115;
+    $b = 30;
+
+    if ($a == 15) {
+    print "<br>this is right answer!!!";
+    }
+    else{
+    print "this is wrong answer!!!";
+    }
+ ?>

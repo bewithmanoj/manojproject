3f4af8ed13234cc6c675561613da636cde727724
Manoj-PC\Manoj <manoj.kumar@web2byte.com>
2017-05-02 11:57:00 +0530

    testfolder

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php  
+	$x = 5 + 6;
+	$text1 = "<br>this is sample var";
+
+	echo "<br>$x" .$text1. "!";
+	?>
+
+<?php 
+	function myfunction() {  
+		static $x = 0;
+		echo $x;
+		$x++;
+	}
+	echo "<br>";
+	myfunction();
+	echo "<br>";
+	myfunction();
+	echo "<br>";
+	myfunction();
+	echo "<br>";
+# this is single line comment in php 
+/* this milti
+	line comment */
+
+
+$a = 115;
+$b = 30;
+
+if ($a == 15) {
+	print "<br>this is right answer!!!";
+}
+else{
+	print "this is wrong answer!!!";
+}
+
+?>
+
+</body>
+</html>
